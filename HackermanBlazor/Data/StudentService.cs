0fbd7726fc6d8d9bf57fa29eeb829300cbb5cdf9
Loke Bendtsen <lokebendtsen@gmail.com>
2023-06-19 10:48:20 +0200

    Add project files.

---
+++ HackermanBlazor/Data/StudentService.cs
@@ -0,0 +1,82 @@
+﻿using System.Data.SqlClient;
+
+namespace HackermanBlazor.Data
+{
+    public class StudentService
+    {
+        private string conString = "Data Source=192.168.1.2;Initial Catalog=MyDB;User ID=sa;Password=Passw0rd;";
+        
+
+        public List<Students> ReadStudents(string query)
+        {
+            List<Students> list = new();
+            using (SqlConnection con = new(conString))
+            {
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Students", con);
+                con.Open();
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Students student = new();
+                    student.Id = (int)reader[0];
+                    student.FirstName = (string)reader[1];
+                    student.LastName = (string)reader[2];
+                    student.Age = (int)reader[3];
+                    student.City = (string)reader[4];
+                    student.Road = (string)reader[5];
+                    student.Hobby = (string)reader[6];
+                    list.Add(student);
+                }
+                con.Close();
+            }
+            return list;
+        }
+
+        public void deleteRow(int id)
+        {
+            using (SqlConnection con = new(conString))
+            {
+                SqlCommand cmd = new SqlCommand("Delete FROM Students WHERE Id=" + id, con);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+        public void CreateStudents(Students st)
+        {
+            using(SqlConnection con = new(conString)) 
+            {
+                string query = $"INSERT INTO Students (FirstName, LastName, Age, City, Road, Hobby) VALUES (@FirstName, @LastName, @Age, @City, @Road, @Hobby)";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.Add("@FirstName",System.Data.SqlDbType.NVarChar).Value = st.FirstName;
+                if (st.FirstName == null) st.FirstName = "First Name not specified";
+                cmd.Parameters.Add("@LastName", System.Data.SqlDbType.NVarChar).Value = st.LastName;
+                if (st.LastName == null) st.LastName = "Last Name not specified";
+                cmd.Parameters.Add("@Age", System.Data.SqlDbType.NVarChar).Value = st.Age;
+                cmd.Parameters.Add("@City", System.Data.SqlDbType.NVarChar).Value = st.City;
+                if (st.City == null) st.City = "City not specified";
+                cmd.Parameters.Add("@Road", System.Data.SqlDbType.NVarChar).Value = st.Road;
+                if (st.Road == null) st.Road = "Road not specified";
+                cmd.Parameters.Add("@Hobby", System.Data.SqlDbType.NVarChar).Value = st.Hobby;
+                if (st.Hobby == null) st.Hobby = "Hobby not specified";
+
+
+                con.Open();
+                try
+                {
+                    cmd.ExecuteNonQuery();
+
+                }
+                catch (Exception )
+                {
+
+                    throw;
+                }
+                con.Close();
+            }
+        }
+
+    }
+}
---
+++ HackermanBlazor/Data/Students.cs
@@ -0,0 +1,28 @@
+﻿namespace HackermanBlazor.Data
+{
+    public class Students
+    {
+        public Students()
+        {
+        }
+
+        public Students(int id, string? firstName, string? lastName, int age, string? city, string? road, string? hobby)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            City = city;
+            Road = road;
+            Hobby = hobby;
+        }
+
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public int Age { get; set; }
+        public string? City { get; set; }
+        public string? Road { get; set; }
+        public string? Hobby { get; set; }
+    }
+}
